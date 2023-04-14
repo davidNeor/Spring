a649ad8654e96cff3200c68f572433fbb2e3c17b
davidNeor <129073454+davidNeor@users.noreply.github.com>
2023-04-14 14:21:31 +0200

    Delete DAA_TiendaIncienso2023_2/src/main/java/rf/com/tienda directory

--- DAA_TiendaIncienso2023_2/src/main/java/rf/com/tienda/interfaces/daos/IUsuario.java
+++
@@ -1,9 +0,0 @@
-package rf.com.tienda.interfaces.daos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import rf.com.tienda.dominio.Usuario;
-
-public interface IUsuario extends JpaRepository<Usuario, Integer> {
-
-}
--- DAA_TiendaIncienso2023_2/src/main/java/rf/com/tienda/interfaces/daos/iPedidoCarrito.java
+++
@@ -1,9 +0,0 @@
-package rf.com.tienda.interfaces.daos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import rf.com.tienda.dominio.PedidoCarrito;
-
-public interface iPedidoCarrito extends JpaRepository<PedidoCarrito, Integer> {
-
-}
